fff4735b041efbe0e2c55e75699cf6d89e8599b9
AlanAriRod <ariasalan163@gmail.com>
2025-05-29 12:57:54 -0600

    Login
    Login sin barra

--- gestoraaa/src/app/app.component.ts
+++ gestoraaa/src/app/app.component.ts
@@ -1,13 +1,21 @@
 // src/app/app.component.ts
 import { Component } from '@angular/core';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [SidebarComponent, RouterOutlet],
+  imports: [SidebarComponent, RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {}
+export class AppComponent {
+  constructor(public router: Router) {}
+
+  // Verifica si estamos en la ruta login
+  isLoginRoute(): boolean {
+    return this.router.url === '/login';
+  }
+}
